525adf258b3340845f0c8fe04a4489757cbd350d
tianweihan <wilhantian@WilhanTiandeMacBook-Pro.local>
2016-05-06 20:12:20 +0800

    ing

--- catui/Core/UIControl.lua
+++ catui/Core/UIControl.lua
@@ -1,11 +1,22 @@
 local UIControl = class("UIControl", {
     x = 0,
     y = 0,
+    -- anchorX = 0,
+    -- anchorY = 0,
+    -- scaleX = 1,
+    -- scaleY = 1,
     width = 0,
     height = 0,
     depth = 0,
     children = {},
     parent = nil,
+    visible = true,
+    enabled = true,
+    childrenEnabled = true,
+
+    -- 自定义参数
+    tag = nil,
+    userData = nil,
 })
 
 ---------------------------------------
@@ -15,15 +26,44 @@ function UIControl:init()
 end
 
 ---------------------------------------
+-- 设置属性
+---------------------------------------
+function UIControl:set(attrs)
+    for k,v in pairs(attrs) do
+        self[k] = v
+    end
+end
+
+---------------------------------------
 -- 更新
 ---------------------------------------
 function UIControl:update(dt)
 end
 
 ---------------------------------------
+-- 系统更新 由UIManager调用
+---------------------------------------
+function UIControl:doUpdate(dt)
+    self:update(dt)
+    for i,v in ipairs(self.children) do
+        v:update(dt)
+    end
+end
+
+---------------------------------------
 -- 渲染
 ---------------------------------------
 function UIControl:draw()
+end
+
+---------------------------------------
+-- 系统渲染 由UIManager调用
+---------------------------------------
+function UIControl:doDraw()
+    self:draw(dt)
+    for i,v in ipairs(self.children) do
+        v:draw(dt)
+    end
 end
 
 ---------------------------------------
@@ -69,7 +109,7 @@ function UIControl:globalToLocal(x, y)
 end
 
 ---------------------------------------
---
+-- 添加一个子控件
 ---------------------------------------
 function UIControl:addChild(child, depth)
     child.parent = self
@@ -77,7 +117,7 @@ function UIControl:addChild(child, depth)
 end
 
 ---------------------------------------
---
+-- 移除一个子控件
 ---------------------------------------
 function UIControl:removeChild(child)
     for i,v in ipairs(self.children) do
@@ -102,18 +142,21 @@ end
 
 ---------------------------------------
 -- 鼠标移动
+-- x, y 为世界坐标
 ---------------------------------------
-function UIControl:mouseMove()
+function UIControl:mouseMove(x, y, dx, dy)
 end
 
 ---------------------------------------
 -- 鼠标按下
+-- x, y 为世界坐标
 ---------------------------------------
 function UIControl:mouseDown()
 end
 
 ---------------------------------------
 -- 鼠标抬起
+-- x, y 为世界坐标
 ---------------------------------------
 function UIControl:mouseUp()
 end
@@ -123,6 +166,18 @@ end
 ---------------------------------------
 function UIControl:setFocus()
     UIManager:getInstance():setFocus(self)
+end
+
+---------------------------------------
+-- 控件获得焦点
+---------------------------------------
+function UIControl:focus()
+end
+
+---------------------------------------
+-- 控件失去焦点
+---------------------------------------
+function UIControl:unFocus()
 end
 
 return UIControl
--- catui/Core/UIManager.lua
+++ catui/Core/UIManager.lua
@@ -1,9 +1,84 @@
 local UIManager = class("UIManager", {
     hoverCtrl = nil,
-    focusCtrl = nil
+    focusCtrl = nil,
+    rootCtrl = nil,
 })
 
+---------------------------------------
+-- 构造
+---------------------------------------
 function UIManager:init()
+end
+
+---------------------------------------
+-- UI更新
+---------------------------------------
+function UIManager:doUpdate(dt)
+    if self.rootCtrl then
+        self.rootCtrl:doUpdate(dt)
+    end
+end
+
+---------------------------------------
+-- 渲染
+---------------------------------------
+function UIManager:doDraw(dt)
+    if self.rootCtrl then
+        self.rootCtrl:doDraw(dt)
+    end
+end
+
+---------------------------------------
+-- 鼠标移动
+---------------------------------------
+function UIManager:mouseMove(x, y, dx, dy)
+    if not rootCtrl then
+        return
+    end
+
+    -- 派发焦点控件的移动事件
+    if self.focusCtrl then
+        self.focusCtrl:mouseMove(x, y, dx, dy)
+    end
+
+    local hitCtrl = self.rootCtrl:hitTest(x, y)
+
+    -- 尝试探测鼠标进入与离开事件
+    if hitCtrl ~= self.hoverCtrl then
+        if self.hoverCtrl then
+            self.hoverCtrl:mouseLeave()
+        end
+
+        self.hoverCtrl = hitCtrl
+
+        if hitCtrl then
+            hitCtrl:mouseEnter()
+        end
+    end
+
+    -- 派发悬浮控件的移动事件
+    if self.hoverCtrl then
+        self.hoverCtrl:mouseMove(x, y, dx, dy)
+    end
+end
+
+---------------------------------------
+-- 设置焦点
+---------------------------------------
+function UIManager:setFocus(ctrl)
+    if self.focusCtrl == ctrl then
+        return
+    end
+
+    if self.focusCtrl then
+        self.focusCtrl:unFocus()
+    end
+
+    self.focusCtrl = ctrl
+
+    if self.focusCtrl then
+        self.focusCtrl:focus()
+    end
 end
 
 return UIManager
